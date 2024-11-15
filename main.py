4b8b76b47dbcf9e9e25ce426ce5a9cb0b7257aef
logicallaw <logicallawbio@gmail.com>
2024-11-15 12:20:29 +0900

    feat(main): Flutter /my-image 구현

--- main.py
+++ main.py
@@ -1,13 +1,21 @@
 from flask import Flask, request, jsonify, send_file
 from flask_cors import CORS
-from PIL import Image
-import io
 app = Flask(__name__)
 CORS(app)
 
-@app.route('/', methods=['GET'])
-def generate_image():
-    return "Hello Python Flask!"
+app_port = 5005
 
-if __name__ == '__main__':
-    app.run(port=5000)
+@app.route('/', methods=['GET'])
+def get_server():
+    return jsonify("Hello Flask Server!")  # JSON 형식으로 응답
+
+@app.route('/user-image', methods=['GET'])
+def get_user_name():
+    # 응답 바디에 JSON 형식으로 message 포함
+    response = {
+        "message": "pretty!"
+    }
+    return jsonify(response)
+
+if __name__ == "__main__":
+    app.run(host="127.0.0.1", port=app_port)
